d93ecef1a5e7f3af0c7fed3dd9c832e365aafd3f
NimeLuck <konstantsokolov@mail.ru>
2025-07-08 23:04:23 +0200

    prvi komit sa standard ConsoleApp

---
+++ KonstantinSokolov/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace KonstantinSokolov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world!");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
