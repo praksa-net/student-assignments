16fb0790273eba03b9324e8c68c4d622ad1fc61c
Djordje Gajic <gajic.djordje0709007@gmail.com>
2025-07-09 13:53:38 +0200

    #Merge main and djGajic

---
+++ DjordjeGajic/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Student
+{
+	public Student()
+	{
+		string Ime { get; set; }
+		string Prezime { get; set; }
+		int GodinaRodjenja { get; set; }
+		List<int> Ocene { get; set; }
+
+
+	}
+}
