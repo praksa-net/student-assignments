e8f6fa09e711ccb96de79fc0dd75078e4275e75a
JocaLegenda69 <joca1sovljanski@gmail.com>
2025-07-09 14:38:02 +0200

    patliđan

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ime=Console.ReadLine();
+            string prezme=Console.ReadLine();
+            int godinaRođenja=int.Parse(Console.ReadLine());
+            Student s = new Student(ime, prezme, godinaRođenja);
+            s.Ocena = new List<int> { };
+            Console.WriteLine("Unesi broj ocena");
+            int broj=int.Parse(Console.ReadLine());
+            for(int i=0; i<broj; i++)
+            {
+                Console.WriteLine("Unesi ocenu:");
+                int ocena=int.Parse(Console.ReadLine());
+                s.Ocena.Add(ocena);
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
