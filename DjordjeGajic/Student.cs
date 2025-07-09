7ae54fd5c3dba3ca451256cff93956b854fa98d5
Djordje Gajic <gajic.djordje0709007@gmail.com>
2025-07-09 13:59:02 +0200

    Add OdrediUspeh

--- DjordjeGajic/Student.cs
+++ DjordjeGajic/Student.cs
@@ -22,6 +22,27 @@ public class Student
 			return suma / Ocene.Count;
 		}
 
-
+		void OdrediUspeh()
+		{
+			string prosek;
+			double ocena = IzracunajProsek();
+			if(ocena >= 4.5)
+			{
+				prosek = "Odlican.";
+			}else if(ocena >= 3.5)
+			{
+				prosek = "Vrlo dobar.";
+			}else if(ocena >= 2.5)
+			{
+				prosek = "Dobar.";
+			}else if(ocena >= 1.5)
+			{
+				prosek = "Dovoljan.";
+			}
+			else
+			{
+				prosek = "Nedovoljan.";
+			}
+		}
 	}
 }
