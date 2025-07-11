57267fc567a3ade9062b9694623616f012ff9be7
Djordje Gajic <gajic.djordje0709007@gmail.com>
2025-07-11 13:44:49 +0200

    move main logic to StudentApp

--- DjordjeGajic/Program.cs
+++ DjordjeGajic/Program.cs
@@ -4,20 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Zdravo, DjordjeGajic!");
-        List<int> ocene = new List<int>();
-        int unos = 1;
-        do
-        {
-            Console.WriteLine("Unesite ocenu (ili '0' za kraj unosa).");
-            unos = Validator.Validacija();
-            int ocena = unos;
-            if (ocena != 0)
-            {
-                ocene.Add(ocena);
-            }
-        } while (unos != 0);
-        Student student = new Student("Djordje", "Gajic", 2007, ocene);
+        StudentApp app = new StudentApp();
         Console.ReadKey();
     }
 
---
+++ DjordjeGajic/StudentApp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DjordjeGajic;
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class StudentApp
+{
+
+
+	public StudentApp()
+	{
+        List<int> ocene = new List<int>();
+        int unos = 1;
+        do
+        {
+            Console.WriteLine("Unesite ocenu (ili '0' za kraj unosa).");
+            unos = Validator.Validacija();
+            int ocena = unos;
+            if (ocena != 0)
+            {
+                ocene.Add(ocena);
+            }
+        } while (unos != 0);
+        Student student = new Student("Djordje", "Gajic", 2007, ocene);
+    }
+}
