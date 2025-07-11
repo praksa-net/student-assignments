f6761a8853d02443acb7e1484599d73182e7a6b8
Djordje Gajic <gajic.djordje0709007@gmail.com>
2025-07-11 14:17:11 +0200

    add StudentFactory

---
+++ DjordjeGajic/StudentFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DjordjeGajic
+{
+    internal class StudentFactory
+    {
+        public static Student CreateMockStudent()
+        {
+            return new Student("Name", "Lastname", 18, new List<int> { 1, 2, 3, 4, 5 });
+        }
+    }
+}
