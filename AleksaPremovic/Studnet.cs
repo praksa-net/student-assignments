449f1d68d0b7a9103199d4214493b2b381bb918c
Aleksa Premović <apremovic@smart.edu.rs>
2025-07-11 13:50:40 +0200

    Dodajem nove fajlove

---
+++ AleksaPremovic/Studnet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Student
+{
+    public string Ime { get; set; }
+    public string Prezime { get; set; }
+    public int GodinaRodjenja { get; set; }
+    public List<int> Ocene { get; set; }
+
+    public Student()
+    {
+        Ocene = new List<int>();
+    }
+}
